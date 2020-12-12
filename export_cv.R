707a86089c9129417707721ff2b2484a6b87fd7b
pathilink <pathilink@gmail.com>
2020-12-12 02:55:54 -0300

    add English cv

--- export_cv.R
+++ export_cv.R
@@ -1,8 +1,8 @@
 export_cv <- function(){
   
-  rmarkdown::render("/home/pathilink/Área de Trabalho/Github/cv/resume.Rmd")
+  rmarkdown::render("/home/pathilink/Área de Trabalho/tracked_files/cv/resume.Rmd")
   
-  pagedown::chrome_print("/home/pathilink/Área de Trabalho/Github/cv/resume.html")
+  pagedown::chrome_print("/home/pathilink/Área de Trabalho/tracked_files/cv/resume.html")
 }
 
 export_cv()
